7be1ef6cf677ec7e7a8a7a8b5381fdfdc604436e
RajaniMadeti <madeti@uwm.edu>
2018-11-05 19:54:28 -0600

    Grunt with nodemon

--- Midterm/server/Gruntfile.js
+++ Midterm/server/Gruntfile.js
@@ -1,31 +1,32 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Project configuration.
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-  
-      env : {
-        dev : {
-          NODE_ENV : 'development'
+        pkg: grunt.file.readJSON('package.json'),
+
+        env: {
+            dev: {
+                NODE_ENV: 'development'
+            },
+            production: {
+                NODE_ENV: 'production'
+            }
         },
-        production: {
-          NODE_ENV : 'production'
-        }
-       }
+        nodemon: {
+            dev: { script: 'index.js' }
+        },
+
+
+
     });
+    grunt.loadNpmTasks('grunt-contrib-nodemon');
 
 
-  grunt.loadNpmTasks('grunt-contrib-nodemon');
-  grunt.loadNpmTasks('grunt-env');
-
-  grunt.registerTask('default',  [
-      'env:dev',
-      'nodemon'
-
+    grunt.registerTask('default', [
+        'nodemon'
     ]);
-     grunt.registerTask('production',  [
-      'env:production',
-      'nodemon'
-      
+    grunt.registerTask('production', [
+        'env:production',
+        'nodemon'
     ]);
+
 };
